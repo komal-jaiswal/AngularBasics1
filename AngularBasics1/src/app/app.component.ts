d4b4fb742cf5c89706a4e0e5fb79779c2f8a1e9f
komal-jaiswal <jaiswalkomal18@gmail.com>
2020-01-18 21:35:04 +0530

    basics of constructor initialization

--- AngularBasics1/src/app/app.component.ts
+++ AngularBasics1/src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
  template:`<h1>{{title}}</h1>
  <span>{{subtitle}}</span>
  <h1>dreamcompanies</h1>
+ <p *ngIf="dreamcompanies.length > 3">You have more than three dream companies</p>
  <span>{{firstPreference}}</span>
  <ul>
- <li *ngFor="let company of dreamcompanies">{{company}}</li>
+ <li *ngFor="let company of dreamcompanies">{{company.companyName}}</li>
  </ul>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title:string;
   subtitle:string;
-  dreamcompanies:string[]=['google','miscrosoft','linkedin','bosch'];
-  firstPreference=this.dreamcompanies[0];
+  dreamcompanies:Hero[]=[
+    new Hero(1,"google"),
+    new Hero(2,'microsoft'),
+    new Hero(3,'linkedin'),
+    new Hero(4,'bosch')
+  ];
+  firstPreference=this.dreamcompanies[0].companyName;
   constructor(){
     this.title = 'AngularBasics1';
     this.subtitle='interpolation';
